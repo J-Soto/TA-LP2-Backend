dfbcdd9d5865e2e5a78b8da4b0f27eac3857ed65
J-Soto <77949690+J-Soto@users.noreply.github.com>
2021-06-05 17:04:44 -0500

    Delete Backend/DatmaSoftController directory

--- Backend/DatmaSoftController/src/pe/datma/datmasoft/config/DBManager.java
+++
@@ -1,8 +0,0 @@
-package pe.datma.datmasoft.config;
-
-public class DBManager {
-    public static String url = "jdbc:mysql:"
-            + "//dbexplosioniquitos.csnhgoov3jct.us-east-1.rds.amazonaws.com:3306/lp2";
-    public static String user = "admin";
-    public static String password = "GuaniraGOD";
-}
