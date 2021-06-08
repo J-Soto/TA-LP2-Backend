11c5e2b3d817ecb0425b06beeb4c589ac7a8a88f
MauricioGMx <gmaldonado@pucp.edu.pe>
2021-06-08 12:39:14 -0500

    class-grupo

--- Backend/DatmaSoftController/src/pe/datma/datmasoft/dao/GrupoDAO.java
+++ Backend/DatmaSoftController/src/pe/datma/datmasoft/dao/GrupoDAO.java
@@ -3,10 +3,24 @@ package pe.datma.datmasoft.dao;
 import pe.datma.datmasoft.familias.Grupo;
 
 import java.util.ArrayList;
+import pe.datma.datmasoft.rrhh.Psicologo;
+import pe.datma.datmasoft.rrhh.Tutor;
 
 public interface GrupoDAO {
-    int insertar(Grupo grupo, int idModulo);
-    int modificar(Grupo grupo);
-    int eliminar(int idGrupo);
-    ArrayList<Grupo> listar(int idGrupo);
+    int insertarGrupo(int idCurso, Grupo grupo);
+    int modificarGrupo(Grupo grupo);
+    int eliminarGrupo(int idGrupo);
+    ArrayList<Grupo> listarGrupoPorIdCurso(int idCurso);
+    
+    //grupo-tutor:
+    int insertarGrupoTutor(int idTutor, int idGrupo, int numInfantes);
+    int modificarGrupoTutor(int idTutor, int idGrupo, int numInfantes);
+    int eliminarGrupoTutor(int idTutor,int idGrupo);
+    ArrayList<Tutor> listarTutoresPorIdGrupo(int idGrupo);
+    
+    //grupo-psicologo:
+    int insertarGrupoPsicologo(int idPsicologo, int idGrupo);
+    int eliminarGrupoPsicologo(int idPsicologo, int idGrupo);
+    ArrayList<Psicologo> listarPsicologosPorIdGrupo(int idGrupo);
+    
 }
--- Backend/DatmaSoftController/src/pe/datma/datmasoft/mysql/GrupoMySQL.java
+++ Backend/DatmaSoftController/src/pe/datma/datmasoft/mysql/GrupoMySQL.java
@@ -6,6 +6,9 @@ import pe.datma.datmasoft.familias.Grupo;
 
 import java.sql.*;
 import java.util.ArrayList;
+import pe.datma.datmasoft.rrhh.Distrito;
+import pe.datma.datmasoft.rrhh.Psicologo;
+import pe.datma.datmasoft.rrhh.Tutor;
 
 public class GrupoMySQL implements  GrupoDAO{
     Connection con;
@@ -13,7 +16,7 @@ public class GrupoMySQL implements  GrupoDAO{
     ResultSet rs;
 
     @Override
-    public int insertar(Grupo grupo, int idModulo) {
+    public int insertarGrupo(int idCurso, Grupo grupo) {
         int resultado = 0;
         
         try{
@@ -22,16 +25,17 @@ public class GrupoMySQL implements  GrupoDAO{
             String instruccion = "{call INSERTAR_GRUPO(?,?,?,?)}";
             cst = con.prepareCall(instruccion);
             
-            cst.registerOutParameter("idgrupo", java.sql.Types.INTEGER); 
-            cst.setInt("fidcurso", idModulo);
+            cst.registerOutParameter("_idgrupo", java.sql.Types.INTEGER); 
+            cst.setInt("_fidcurso2", idCurso);
             cst.setString("_nombre", grupo.getNombrePromocion());
             cst.setInt("_maxtutores", grupo.getMaxCantCuidadores());
-           
             cst.executeUpdate();
-            grupo.setIdGrupo(cst.getInt("idgrupo"));
+            
+            grupo.setIdGrupo(cst.getInt("_idgrupo"));
+            
             cst.close();
             con.close();       
-            resultado = 1;
+            resultado = cst.getInt("_idgrupo");
         }catch(Exception ex){
             System.out.println(ex.getMessage());
         }finally{
@@ -41,7 +45,7 @@ public class GrupoMySQL implements  GrupoDAO{
     }
 
     @Override
-    public int modificar(Grupo grupo) {
+    public int modificarGrupo(Grupo grupo) {
         int resultado = 0;
         
         try{
@@ -49,10 +53,11 @@ public class GrupoMySQL implements  GrupoDAO{
             con = DriverManager.getConnection(DBManager.url,DBManager.user,DBManager.password);
             String instruccion = "{call MODIFICAR_GRUPO(?,?,?)}";
             cst = con.prepareCall(instruccion);
-            cst.setInt("_id_grupo", grupo.getIdGrupo()); //id_persona
+            cst.setInt("_idgrupo", grupo.getIdGrupo()); 
             cst.setString("_nombre", grupo.getNombrePromocion());
             cst.setInt("_maxtutores", grupo.getMaxCantCuidadores());
             cst.executeUpdate();
+            
             cst.close();
             con.close();       
             resultado = 1;
@@ -65,15 +70,16 @@ public class GrupoMySQL implements  GrupoDAO{
     }
 
     @Override
-    public int eliminar(int idGrupo) {
+    public int eliminarGrupo(int idGrupo) {
         int resultado = 0;
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
             con = DriverManager.getConnection(DBManager.url,DBManager.user,DBManager.password);
             String instruccion = "{call ELIMINAR_GRUPO(?)}";
             cst = con.prepareCall(instruccion);
-            cst.setInt("_id_grupo",idGrupo);
+            cst.setInt("_idgrupo",idGrupo);
             cst.executeUpdate();
+            
             cst.close();
             con.close();       
             resultado = 1;
@@ -86,18 +92,23 @@ public class GrupoMySQL implements  GrupoDAO{
     }
 
     @Override
-    public ArrayList<Grupo> listar(int idGrupo) {
+    public ArrayList<Grupo> listarGrupoPorIdCurso(int idCurso) {
         ArrayList<Grupo> grupos = new ArrayList<>();
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
             con = DriverManager.getConnection(DBManager.url,DBManager.user,DBManager.password);
-            String instruccion = "{call LISTAR_GRUPO()}";
+            String instruccion = "{call LISTAR_GRUPO(?)}";
             cst = con.prepareCall(instruccion);
+            cst.setInt("_idcurso", idCurso);
             rs = cst.executeQuery();   
+            
             while(rs.next()){
-                Grupo  grupo = new Grupo();
-                grupo.setNombrePromocion(rs.getString("nombre"));
+                Grupo grupo = new Grupo();
+                
+                grupo.setIdGrupo(rs.getInt("idgrupo"));
+                grupo.setNombrePromocion(rs.getString("nombre_grupo"));
                 grupo.setMaxCantCuidadores(rs.getInt("maxtutores"));
+                
                 grupos.add(grupo);
             }
             cst.close();
@@ -110,5 +121,242 @@ public class GrupoMySQL implements  GrupoDAO{
         }
         return grupos;
     }
+    
+    
+    ////////////////
+    //grupo-tutor:
+
+    @Override
+    public int insertarGrupoTutor(int idTutor, int idGrupo, int numInfantes) {
+        int resultado = 0;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(DBManager.url,DBManager.user,DBManager.password);
+            String instruccion = "{call INSERTAR_GRUPO_TUTOR(?,?,?,?)}";
+            cst = con.prepareCall(instruccion);
+            cst.registerOutParameter("_idgrupo_tutor", java.sql.Types.INTEGER);
+            cst.setInt("_fidtutor", idTutor);
+            cst.setInt("_fidgrupo2", idGrupo);
+            cst.setInt("_nroinfantes", numInfantes);
+            cst.executeUpdate();
+            resultado = cst.getInt("_idgrupo_tutor");
+            
+            cst.close();
+            con.close();
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }finally{
+            try{ con.close(); }catch(Exception ex){}
+        }
+        return resultado;
+    }
+
+    @Override
+    public int modificarGrupoTutor(int idTutor, int idGrupo, int numInfantes) {
+        int resultado = 0;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(DBManager.url,DBManager.user,DBManager.password);
+            String instruccion = "{call MODIFICAR_GRUPO_TUTOR(?,?,?)}";
+            cst = con.prepareCall(instruccion);
+            cst.setInt("_idtutor", idTutor);
+            cst.setInt("_idgrupo", idGrupo);
+            cst.setInt("_nroinfantes", numInfantes);
+            cst.executeUpdate();
+            
+            cst.close();
+            con.close();
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }finally{
+            try{ con.close(); }catch(Exception ex){}
+        }
+        return resultado;
+    }
+
+    @Override
+    public int eliminarGrupoTutor(int idTutor, int idGrupo) {
+        int resultado = 0;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(DBManager.url,DBManager.user,DBManager.password);
+            String instruccion = "{call ELIMINAR_TUTOR_DE_GRUPO(?,?)}";
+            cst = con.prepareCall(instruccion);
+            cst.setInt("_idtutor", idTutor);
+            cst.setInt("_idgrupo", idGrupo);
+            cst.executeUpdate();
+            
+            cst.close();
+            con.close();
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }finally{
+            try{ con.close(); }catch(Exception ex){}
+        }
+        return resultado;
+    }
+
+    @Override
+    public ArrayList<Tutor> listarTutoresPorIdGrupo(int idGrupo) {
+        ArrayList<Tutor> tutores = new ArrayList<>();
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(DBManager.url,DBManager.user,DBManager.password);
+            String instruccion = "{call LISTAR_TUTORES_POR_ID_GRUPO(?)}";
+            cst = con.prepareCall(instruccion);
+            cst.setInt("_idgrupo", idGrupo);
+            rs = cst.executeQuery();
+            
+            while(rs.next()){
+                Tutor tutor = new Tutor();
+                tutor.setIdPersona(rs.getInt("idpersona"));
+                tutor.setDNI(rs.getString("dni"));
+                tutor.setNombre(rs.getString("nombre"));
+                tutor.setApellidoPaterno(rs.getString("apellidopaterno"));
+                tutor.setApellidoMaterno(rs.getString("apellidomaterno"));
+                tutor.setFechaNacimiento(rs.getDate("fechanacimiento"));
+                tutor.setGenero(rs.getString("genero").charAt(0));
+                tutor.setEdad(rs.getInt("edad"));
+                tutor.setTelefono(rs.getString("telefono"));
+                tutor.setCelular(rs.getString("celular"));
+                tutor.setCorreo(rs.getString("correo"));
+                tutor.setDistrito(new Distrito());
+                tutor.getDistrito().setIdDistrito(rs.getInt("iddistrito"));
+                tutor.getDistrito().setNombre(rs.getString("nombre_distrito"));
+                tutor.setIdUsuario(rs.getInt("idusuario"));
+                tutor.setUser(rs.getString("user"));
+                tutor.setPassword(rs.getString("password"));
+                tutor.setTipo(rs.getInt("tipo"));
+                tutor.setFotoPerfil(rs.getBytes("foto"));
+                tutor.setGestante(rs.getInt("gestante"));
+                tutor.setDia(rs.getInt("preferenciaDias"));
+                tutor.setTurno(rs.getInt("preferenciaTurno"));
+                tutor.setBajoRecursos(rs.getInt("bajosrecursos"));
+                tutor.setTiposConexion(rs.getInt("tipointernet"));
+                tutor.setRedesSociales(rs.getInt("redsocial"));
+                tutor.setDispositivos(rs.getInt("tipodispositivo"));
+                tutor.setGrupos(new ArrayList<Grupo>());
+                Grupo grupo = new Grupo();
+                grupo.setIdGrupo(idGrupo);
+                grupo.setCantInfantes(rs.getInt("nroinfantes"));
+                tutor.getGrupos().add(grupo); //Este tutor pertenece al grupo con el id recibido
+                tutor.setActivo(1);
+                tutor.setActivoTutor(1); 
+                tutores.add(tutor);
+            }
+            
+            cst.close();
+            con.close();
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }finally{
+            try{ con.close(); }catch(Exception ex){}
+        }
+        
+        return tutores;
+    }
+
+    ///////////////////
+    //grupo-psicologo:
+
+    @Override
+    public int insertarGrupoPsicologo(int idPsicologo, int idGrupo) {
+        int resultado = 0;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(DBManager.url,DBManager.user,DBManager.password);
+            String instruccion = "{call INSERTAR_GRUPO_PSICOLOGO(?,?,?)}";
+            cst = con.prepareCall(instruccion);
+            cst.registerOutParameter("_idgrupo_psicologo", java.sql.Types.INTEGER);
+            cst.setInt("_fidpsicologo", idPsicologo);
+            cst.setInt("_fidgrupo", idGrupo);
+            cst.executeUpdate();
+            resultado = cst.getInt("_idgrupo_psicologo");
+            
+            cst.close();
+            con.close();
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }finally{
+            try{ con.close(); }catch(Exception ex){}
+        }
+        return resultado;
+    }
+
+    
+    @Override
+    public int eliminarGrupoPsicologo(int idPsicologo, int idGrupo) {
+        
+        int resultado = 0;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(DBManager.url,DBManager.user,DBManager.password);
+            String instruccion = "{call ELIMINAR_PSICOLOGO_DE_GRUPO(?,?)}";
+            cst = con.prepareCall(instruccion);
+            cst.setInt("_idpsicologo", idPsicologo);
+            cst.setInt("_idgrupo", idGrupo);
+            cst.executeUpdate();
+            
+            resultado = 1;
+            cst.close();
+            con.close();
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }finally{
+            try{ con.close(); }catch(Exception ex){}
+        }
+        return resultado;
+    }
+
+    @Override
+    public ArrayList<Psicologo> listarPsicologosPorIdGrupo(int idGrupo) {
+        ArrayList<Psicologo> psicologos = new ArrayList<>();
+        
+         try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(DBManager.url,DBManager.user,DBManager.password);
+            String instruccion = "{call LISTAR_PSICOLOGOS_POR_ID_GRUPO(?)}";
+            cst = con.prepareCall(instruccion);
+            cst.setInt("_idgrupo", idGrupo);
+            rs = cst.executeQuery();
+            
+            while(rs.next()){
+                Psicologo psicologo = new Psicologo();
+                psicologo.setIdPersona(rs.getInt("idpersona"));
+                psicologo.setDNI(rs.getString("dni"));
+                psicologo.setNombre(rs.getString("nombre"));
+                psicologo.setApellidoPaterno(rs.getString("apellidopaterno"));
+                psicologo.setApellidoMaterno(rs.getString("apellidomaterno"));
+                psicologo.setFechaNacimiento(rs.getDate("fechanacimiento"));
+                psicologo.setGenero(rs.getString("genero").charAt(0));
+                psicologo.setEdad(rs.getInt("edad"));
+                psicologo.setTelefono(rs.getString("telefono"));
+                psicologo.setCelular(rs.getString("celular"));
+                psicologo.setCorreo(rs.getString("correo"));
+                psicologo.setDistrito(new Distrito());
+                psicologo.getDistrito().setIdDistrito(rs.getInt("iddistrito"));
+                psicologo.getDistrito().setNombre(rs.getString("nombre_distrito"));
+                psicologo.setIdUsuario(rs.getInt("idusuario"));
+                psicologo.setUser(rs.getString("user"));
+                psicologo.setPassword(rs.getString("password"));
+                psicologo.setTipo(rs.getInt("tipo"));
+                psicologo.setFotoPerfil(rs.getBytes("foto"));
+                psicologo.setActivo(1);
+                psicologo.setActivoPsicologo(1); 
+                
+                psicologos.add(psicologo);
+            }
+            
+            cst.close();
+            con.close();
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }finally{
+            try{ con.close(); }catch(Exception ex){}
+        }
+         
+        return psicologos;
+    }
    
+    
 }
