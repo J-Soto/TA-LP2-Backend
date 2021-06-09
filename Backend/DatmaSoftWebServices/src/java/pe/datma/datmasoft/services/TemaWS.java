788741ecfdebd170dce7bc951dcb038f4b376e68
John Soto <john.soto@pucp.edu.pe>
2021-06-09 00:25:55 -0500

    WS's Veneko

--- Backend/DatmaSoftWebServices/src/java/pe/datma/datmasoft/services/TemaWS.java
+++ Backend/DatmaSoftWebServices/src/java/pe/datma/datmasoft/services/TemaWS.java
@@ -3,14 +3,24 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package pe.datma.datmasoft.services;
 
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+
 /**
-Nombre: John Alexander Soto Tagle
-Codigo: 20161312
-Fecha: 
+ *
+ * @author John
  */
+@WebService(serviceName = "TemaWS")
 public class TemaWS {
 
+    /**
+     * This is a sample web service operation
+     */
+    @WebMethod(operationName = "hello")
+    public String hello(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
 }
