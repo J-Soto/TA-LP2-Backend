3f0f2e1f42921435d09204a9a5cd3fa9fc31390e
John Soto <john.soto@pucp.edu.pe>
2021-06-09 00:14:39 -0500

    MySQL's Veneko

--- Backend/DatmaSoftWebServices/src/java/pe/datma/datmasoft/services/CursoWS.java
+++ Backend/DatmaSoftWebServices/src/java/pe/datma/datmasoft/services/CursoWS.java
@@ -4,7 +4,6 @@
  * and open the template in the editor.
  */
 package pe.datma.datmasoft.services;
-
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
---
+++ Backend/DatmaSoftWebServices/src/java/pe/datma/datmasoft/services/TemaWS.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pe.datma.datmasoft.services;
+
+/**
+Nombre: John Alexander Soto Tagle
+Codigo: 20161312
+Fecha: 
+ */
+public class TemaWS {
+
+}
