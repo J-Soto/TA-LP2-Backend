188b76954a3bd60971ca50a7f7794e3100af7272
GianPRodriguez <gianp.rodriguezp@pucp.pe>
2021-06-09 11:58:20 -0500

    Fin TutorWS

--- Backend/DatmaSoftWebServices/src/java/pe/datma/datmasoft/services/TutorWS.java
+++ Backend/DatmaSoftWebServices/src/java/pe/datma/datmasoft/services/TutorWS.java
@@ -5,9 +5,14 @@
  */
 package pe.datma.datmasoft.services;
 
+import java.util.ArrayList;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
+import pe.datma.datmasoft.dao.TutorDAO;
+import pe.datma.datmasoft.modulos.Actividad;
+import pe.datma.datmasoft.mysql.TutorMySQL;
+import pe.datma.datmasoft.rrhh.Tutor;
 
 /**
  *
@@ -15,12 +20,53 @@ import javax.jws.WebParam;
  */
 @WebService(serviceName = "TutorWS")
 public class TutorWS {
-
-    /**
-     * This is a sample web service operation
-     */
-    @WebMethod(operationName = "hello")
-    public String hello(@WebParam(name = "name") String txt) {
-        return "Hello " + txt + " !";
+    private TutorDAO daoTutor;
+    
+    public TutorWS(TutorDAO daoTutor) {
+        this.daoTutor = new TutorMySQL();
+    }
+    
+    @WebMethod(operationName = "listarTutor")
+    public ArrayList<Tutor> listarTutor() {
+        ArrayList<Tutor> tutores =new ArrayList<Tutor>();
+        try {
+            tutores=daoTutor.listarTutores();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        return tutores;
+    }
+    
+    @WebMethod(operationName = "eliminarTutor")
+    public int eliminarTutor(@WebParam(name = "idTutor") int idTutor, @WebParam(name = "idUsuario") int idUsuario) {
+        int resultado=0;
+        try {
+            resultado=daoTutor.eliminarTutor(idTutor,  idUsuario);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        return resultado;
+    }
+    
+    @WebMethod(operationName = "insertarTutor")
+    public int insertarTutor(@WebParam(name = "tutor") Tutor tutor) {
+        int resultado=0;
+        try {
+            resultado=daoTutor.insertarTutor(tutor);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        return resultado;
+    }
+    
+    @WebMethod(operationName = "modificarTutor")
+    public int modificarTutor(@WebParam(name = "tutor") Tutor tutor) {
+        int resultado=0;
+        try {
+            resultado=daoTutor.modificarTutor(tutor);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        return resultado;
     }
 }
