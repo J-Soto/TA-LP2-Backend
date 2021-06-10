522723413e1fae5afcaf12064cfd69f942104b83
Jose Luis Huertas Panitz <joseluis_7699@outlook.com>
2021-06-10 04:38:16 -0500

    verificarDNI 2.0

--- Backend/DatmaSoftWebServices/src/java/pe/datma/datmasoft/services/PsicologoWS.java
+++ Backend/DatmaSoftWebServices/src/java/pe/datma/datmasoft/services/PsicologoWS.java
@@ -7,6 +7,8 @@ package pe.datma.datmasoft.services;
 
 //import com.google.gson.Gson;
 //import com.google.gson.JsonObject;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -33,53 +35,53 @@ public class PsicologoWS {
         daoPsicologo = new PsicologoMySQL();
     }
     
-//    @WebMethod(operationName = "verificarDNI")
-//    public int verificarDNI(@WebParam(name = "dni") String dni,
-//            @WebParam(name = "nom") String nom,
-//            @WebParam(name = "apPat") String app,
-//            @WebParam(name = "apMat") String apm){
-//        int resultado=0;
-//        String enlace="https://dni.optimizeperu.com/api/persons/"+dni+"?format=json";
-//        //String enlace="https://dni.optimizeperu.com/api/prod/persons/"+dni;
-//        try {
-//            //String param = "?Authorization=" + "k4d2956bd531ab61d44f4fa07304b20e13913815";
-//            URL url = new URL(enlace);
-//            HttpURLConnection con=(HttpURLConnection)url.openConnection();
-//            //con.setRequestProperty("token", "k4d2956bd531ab61d44f4fa07304b20e13913815");
-//            con.setRequestMethod("GET");
-//            
-//            BufferedReader input = new BufferedReader(new InputStreamReader(con.getInputStream()));
-//            
-//            String line = input.readLine(); 
-//
-////            System.out.println(line);
-//            Gson gson=new Gson();
-//            JsonObject json=gson.fromJson(line, JsonObject.class);
-//            String DNI=json.get("dni").toString();
-//            String NOM=json.get("name").toString();
-//            String APP=json.get("first_name").toString();
-//            String APM=json.get("last_name").toString();
-//            
-//            //System.out.println(DNI.length());
-//            DNI=DNI.substring(1,DNI.length()-1);
-//            NOM=NOM.substring(1,NOM.length()-1);
-//            APP=APP.substring(1,APP.length()-1);
-//            APM=APM.substring(1,APM.length()-1);
-//            nom=nom.toUpperCase();
-//            app=app.toUpperCase();
-//            apm=apm.toUpperCase();
-//            if(NOM.equals(nom) && APP.equals(app) && APM.equals(apm) && 
-//                    DNI.equals(dni))
-//                resultado=1;
-////      
-//            con.disconnect();
-//            
-//           
-//        } catch (Exception e) {
-//            System.out.println(e.getMessage());
-//        }
-//        return resultado;
-//    }
+    @WebMethod(operationName = "verificarDNI")
+    public int verificarDNI(@WebParam(name = "dni") String dni,
+            @WebParam(name = "nom") String nom,
+            @WebParam(name = "apPat") String app,
+            @WebParam(name = "apMat") String apm){
+        int resultado=0;
+        String enlace="https://dni.optimizeperu.com/api/persons/"+dni+"?format=json";
+        //String enlace="https://dni.optimizeperu.com/api/prod/persons/"+dni;
+        try {
+            //String param = "?Authorization=" + "k4d2956bd531ab61d44f4fa07304b20e13913815";
+            URL url = new URL(enlace);
+            HttpURLConnection con=(HttpURLConnection)url.openConnection();
+            //con.setRequestProperty("token", "k4d2956bd531ab61d44f4fa07304b20e13913815");
+            con.setRequestMethod("GET");
+            
+            BufferedReader input = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            
+            String line = input.readLine(); 
+
+//            System.out.println(line);
+            Gson gson=new Gson();
+            JsonObject json=gson.fromJson(line, JsonObject.class);
+            String DNI=json.get("dni").toString();
+            String NOM=json.get("name").toString();
+            String APP=json.get("first_name").toString();
+            String APM=json.get("last_name").toString();
+            
+            //System.out.println(DNI.length());
+            DNI=DNI.substring(1,DNI.length()-1);
+            NOM=NOM.substring(1,NOM.length()-1);
+            APP=APP.substring(1,APP.length()-1);
+            APM=APM.substring(1,APM.length()-1);
+            nom=nom.toUpperCase();
+            app=app.toUpperCase();
+            apm=apm.toUpperCase();
+            if(NOM.equals(nom) && APP.equals(app) && APM.equals(apm) && 
+                    DNI.equals(dni))
+                resultado=1;
+//      
+            con.disconnect();
+            
+           
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return resultado;
+    }
     
     @WebMethod(operationName = "insertarPsicologo")
     public int insertarPsicologo(@WebParam(name = "psicologo") Psicologo psicologo){
