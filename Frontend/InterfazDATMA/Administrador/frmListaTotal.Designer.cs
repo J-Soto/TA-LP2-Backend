a77801be19101474d08c7def9daa96ba09858323
Jose Luis Huertas Panitz <joseluis_7699@outlook.com>
2021-06-10 03:32:16 -0500

    dd

--- Frontend/InterfazDATMA/Administrador/frmListaTotal.Designer.cs
+++ Frontend/InterfazDATMA/Administrador/frmListaTotal.Designer.cs
@@ -31,6 +31,8 @@ namespace InterfazDATMA.Administrador
         {
             this.Header = new System.Windows.Forms.Label();
             this.dgvLista = new System.Windows.Forms.DataGridView();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
             this.colNombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colApPat = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colApMat = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -41,8 +43,6 @@ namespace InterfazDATMA.Administrador
             this.colTelf = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colCel = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colEmail = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.txtBuscar = new System.Windows.Forms.TextBox();
-            this.btnBuscar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvLista)).BeginInit();
             this.SuspendLayout();
             // 
@@ -81,8 +81,31 @@ namespace InterfazDATMA.Administrador
             this.dgvLista.Size = new System.Drawing.Size(783, 255);
             this.dgvLista.TabIndex = 1;
             // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(276, 135);
+            this.txtBuscar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(308, 20);
+            this.txtBuscar.TabIndex = 5;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBuscar.Font = new System.Drawing.Font("Century Gothic", 10F);
+            this.btnBuscar.ForeColor = System.Drawing.Color.White;
+            this.btnBuscar.Location = new System.Drawing.Point(622, 131);
+            this.btnBuscar.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(87, 24);
+            this.btnBuscar.TabIndex = 6;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
             // colNombre
             // 
+            this.colNombre.DataPropertyName = "nombre";
             this.colNombre.HeaderText = "Nombre";
             this.colNombre.MinimumWidth = 10;
             this.colNombre.Name = "colNombre";
@@ -169,28 +192,6 @@ namespace InterfazDATMA.Administrador
             this.colEmail.Name = "colEmail";
             this.colEmail.ReadOnly = true;
             this.colEmail.Width = 200;
-            // 
-            // txtBuscar
-            // 
-            this.txtBuscar.Location = new System.Drawing.Point(276, 135);
-            this.txtBuscar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.txtBuscar.Name = "txtBuscar";
-            this.txtBuscar.Size = new System.Drawing.Size(308, 20);
-            this.txtBuscar.TabIndex = 5;
-            // 
-            // btnBuscar
-            // 
-            this.btnBuscar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnBuscar.Font = new System.Drawing.Font("Century Gothic", 10F);
-            this.btnBuscar.ForeColor = System.Drawing.Color.White;
-            this.btnBuscar.Location = new System.Drawing.Point(622, 131);
-            this.btnBuscar.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
-            this.btnBuscar.Name = "btnBuscar";
-            this.btnBuscar.Size = new System.Drawing.Size(87, 24);
-            this.btnBuscar.TabIndex = 6;
-            this.btnBuscar.Text = "Buscar";
-            this.btnBuscar.UseVisualStyleBackColor = true;
-            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
             // 
             // frmListaTotal
             // 
