10e0d52e7bde141fffca910cbaef96a487968e7b
John Soto <john.soto@pucp.edu.pe>
2021-06-10 04:09:23 -0500

    a

--- Frontend/InterfazDATMA/Administrador/frmListaPsicologos.cs
+++ Frontend/InterfazDATMA/Administrador/frmListaPsicologos.cs
@@ -12,9 +12,31 @@ namespace InterfazDATMA.Administrador
 {
     public partial class Header : Form
     {
+        private PsicologoWS.PsicologoWSClient daoPsicologo;
+
         public Header()
         {
             InitializeComponent();
+            daoPsicologo = new PsicologoWS.PsicologoWSClient();
+            dgvPsico.AutoGenerateColumns = false;
+            dgvPsico.DataSource = new BindingList<PsicologoWS.PsicologoWSClient>(daoPsicologo.listarTodosPsicologos());
         }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            BindingList<PsicologoWS.PsicologoWSClient> psicos = new BindingList<PsicologoWS.PsicologoWSClient>(daoPsicologo.listarTodosPsicologos());
+            for (int i = 0; i < psicos.Count; i++)
+            {
+                if (!psicos[i].nombre.Contains(txtBuscar.Text))
+                {
+                    psicos.RemoveAt(i);
+                    i--;
+                }
+            }
+            dgvPsico.DataSource = psicos;
+        }
+
+       
     }
+
 }
