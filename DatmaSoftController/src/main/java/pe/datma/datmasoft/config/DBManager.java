13dd2267336928647423f808bbac9073ed54d486
csantv <csantve@gmail.com>
2021-06-27 01:17:27 -0500

    restaurar bd

--- DatmaSoftController/src/main/java/pe/datma/datmasoft/config/DBManager.java
+++ DatmaSoftController/src/main/java/pe/datma/datmasoft/config/DBManager.java
@@ -11,9 +11,9 @@ package pe.datma.datmasoft.config;
  */
 public class DBManager {
     public static String url = "jdbc:mysql:"
-            + "//lp2.clbfimuoxixc.us-east-1.rds.amazonaws.com:3306/ta";
+            + "//dbexplosioniquitos.csnhgoov3jct.us-east-1.rds.amazonaws.com:3306/lp2";
     public static String user = "admin";
-    public static String password = "3C6JXtzGEZYSZvYqPq65RzprG9LaaPHS";
+    public static String password = "GuaniraGOD";
     
     
 }
